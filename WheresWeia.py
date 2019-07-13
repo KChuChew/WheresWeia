89f364864bce61c1611df4563af92fee0f0b863a
Kevin Chu <kevin.git.chu@gmail.com>
2019-07-13 00:11:34 -0700

    Tested and confirmed stopwords list

--- WheresWeia.py
+++ WheresWeia.py
@@ -3,5 +3,13 @@
 
 # Do not use memes for evil! 
 
+import sys
+import codecs
 import nltk
+from nltk.corpus import stopwords
 
+x = ["Hey, you should xD me in the ass.", "You should yolo", "You should 420 me", "You should just fuckityoloxD"]
+
+ignored_words = nltk.corpus.stopwords.words('english')
+
+print(ignored_words)
